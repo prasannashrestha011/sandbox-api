5c1d58b1c910be184649247833b3c2419e15bc4b
prasannashrestha011 <shresthaprashanna27@gmail.com>
2026-05-09 19:55:01 +0545

    feat: add logging for image pulling in PullImage function

--- actions/image/image_pull.go
+++ actions/image/image_pull.go
@@ -3,6 +3,7 @@ package sandbox_image
 import (
 	"context"
 	"io"
+	"log"
 
 	"github.com/moby/moby/client"
 )
@@ -10,13 +11,12 @@ import (
 func PullImage(ctx context.Context, apiClient *client.Client, imageID string) {
 	_, err := apiClient.ImageInspect(ctx, imageID)
 	if err != nil {
+		log.Println("Pulling image: ", imageID)
 		reader, err := apiClient.ImagePull(ctx, imageID, client.ImagePullOptions{})
-
 		if err != nil {
 			panic(err)
 		}
 		io.Copy(io.Discard, reader)
 
 	}
-
 }
