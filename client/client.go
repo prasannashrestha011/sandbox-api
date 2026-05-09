2fa8117d37298c967ff2181ca723c14158efd168
prasannashrestha011 <shresthaprashanna27@gmail.com>
2026-05-09 19:55:21 +0545

    feat: refactor client.go to organize imports and enhance CreateNewSandBox function

--- client/client.go
+++ client/client.go
@@ -1,9 +1,19 @@
 package sandbox_client
 
-import "github.com/moby/moby/client"
+import (
+	"context"
+	"log"
+	"time"
+
+	sandbox_container "main/actions/container"
+	sandbox_image "main/actions/image"
+	sandbox_request "main/types"
+
+	"github.com/google/uuid"
+	"github.com/moby/moby/client"
+)
 
 func NewSandboxClient() (*client.Client, error) {
-
 	apiClient, err := client.New(
 		client.FromEnv,
 		client.WithUserAgent("my-application/1.0.0"),
@@ -12,4 +22,30 @@ func NewSandboxClient() (*client.Client, error) {
 		return nil, err
 	}
 	return apiClient, nil
+}
+
+func CreateNewSandBox(apiClient *client.Client, req *sandbox_request.CreateRequest) (*sandbox_request.CreateResponse, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), req.SessionTimeout)
+	defer cancel()
+	sandbox_image.PullImage(ctx, apiClient, req.ImageID)
+	containerID, err := sandbox_container.CreateContainer(ctx, apiClient, req)
+	if err != nil {
+		return nil, err
+	}
+	log.Println("Container ID: ", containerID)
+
+	log.Println("Starting the container")
+	_, err = apiClient.ContainerStart(ctx, containerID, client.ContainerStartOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	sessionID, _ := uuid.NewUUID()
+	return &sandbox_request.CreateResponse{
+		ContainerID: containerID,
+		SessionID:   sessionID,
+		Status:      "active",
+		CreatedAt:   time.Now(),
+		ExpiresAt:   time.Now().Add(req.SessionTimeout),
+	}, nil
 }
