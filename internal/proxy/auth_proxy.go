3e2530ef2b7f95bc8f44b3bb2b79dec3afb19193
prasannashrestha011 <shresthaprashanna27@gmail.com>
2026-05-28 19:35:23 +0545

    feat: basic auth proxy with setting user ID in ctx

---
+++ internal/proxy/auth_proxy.go
@@ -0,0 +1,34 @@
+package proxy
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	request_context "main/internal/context"
+	jwtutil "main/internal/security/jwt"
+)
+
+func AuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		cookie, err := r.Cookie("auth_token")
+		if err != nil || cookie.Value == "" {
+			log.Println("Missing auth cookie")
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+		claim, err := jwtutil.JwtUtil.ValidateToken(cookie.Value)
+		if err != nil {
+			http.Error(w, "Invalid access token", http.StatusUnauthorized)
+			return
+		}
+
+		if claim.ExpiresAt != nil && claim.ExpiresAt.Before(time.Now()) {
+			http.Error(w, "Token expired", http.StatusUnauthorized)
+			return
+		}
+		ctx = request_context.WithUserID(ctx, claim.UserID)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
