7255f59eea2451c824a6d1f69575e4a22c587893
prasannashrestha011 <shresthaprashanna27@gmail.com>
2026-05-29 12:13:01 +0545

    chore: deleted old sanbox types

--- internal/sandbox/types/const.go
+++
@@ -1,9 +0,0 @@
-package sandbox_type
-
-// SandboxState represents the lifecycle state of a sandbox.
-type SandboxState string
-
-const (
-	StateActive   SandboxState = "active"
-	StateInActive SandboxState = "inactive"
-)
