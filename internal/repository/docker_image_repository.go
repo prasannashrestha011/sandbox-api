da7491cae948275ef21a1147adaea7446969657b
prasannashrestha011 <shresthaprashanna27@gmail.com>
2026-05-29 17:55:32 +0545

    feat: docker image getter method by id

--- internal/repository/docker_image_repository.go
+++ internal/repository/docker_image_repository.go
@@ -10,6 +10,7 @@ import (
 type DockerImageRepository interface {
 	// Create inserts a new Docker image record into the database.
 	Create(ctx context.Context, image *model.DockerImage) error
+	FindByID(ctx context.Context, id string) (*model.DockerImage, error)
 }
 
 type dockerImageRepository struct {
@@ -23,4 +24,13 @@ func NewDockerImageRepository(db *gorm.DB) DockerImageRepository {
 
 func (r *dockerImageRepository) Create(ctx context.Context, image *model.DockerImage) error {
 	return r.db.WithContext(ctx).Model(&model.DockerImage{}).Create(image).Error
+}
+
+func (r *dockerImageRepository) FindByID(ctx context.Context, id string) (*model.DockerImage, error) {
+	var image model.DockerImage
+	err := r.db.WithContext(ctx).Model(&model.DockerImage{}).Where("id = ?", id).First(&image).Error
+	if err != nil {
+		return nil, err
+	}
+	return &image, nil
 }
