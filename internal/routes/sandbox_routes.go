fe1405c7cf5d2820c82e5ac17d10575bfc366e37
prasannashrestha011 <shresthaprashanna27@gmail.com>
2026-05-29 17:56:17 +0545

    feat: added auth middleware to retrieve user id from request context

--- internal/routes/sandbox_routes.go
+++ internal/routes/sandbox_routes.go
@@ -4,11 +4,13 @@ import (
 	"github.com/go-chi/chi/v5"
 
 	"main/internal/controllers"
+	"main/internal/proxy"
 )
 
 // RegisterSandboxRoutes wires sandbox endpoints into the router.
 func RegisterSandboxRoutes(r chi.Router, controller *controllers.SandboxController) {
 	r.Route("/sandboxes", func(sr chi.Router) {
+		sr.Use(proxy.AuthMiddleware)
 		sr.Post("/", controller.CreateSandbox)
 		sr.Get("/", controller.ListSandboxesByUser)
 		sr.Get("/{id}", controller.GetSandboxByID)
