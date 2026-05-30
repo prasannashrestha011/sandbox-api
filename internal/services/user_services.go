061cd5a02205be0a65451618c972be619f2c8ecb
prasannashrestha011 <shresthaprashanna27@gmail.com>
2026-05-30 16:47:30 +0545

    feat: enhance error handling in user service methods

--- internal/services/user_services.go
+++ internal/services/user_services.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/google/uuid"
 
+	postgres_error "main/internal/infra/postgres"
 	"main/internal/repository"
 	"main/internal/repository/model"
 	"main/internal/security/hashing"
@@ -37,31 +38,42 @@ func (s *userService) Create(ctx context.Context, user *model.User) error {
 
 	hashed, err := hashing.HashPassword(password)
 	if err != nil {
-		return err
+		return postgres_error.MapError(err, "create user", "user")
 	}
 	user.Password = hashed
 
-	return s.userrepo.Create(ctx, user)
+	err = s.userrepo.Create(ctx, user)
+	return postgres_error.MapError(err, "create user", "user")
 }
 
 func (s *userService) GetByID(ctx context.Context, id uuid.UUID) (*model.User, error) {
-	return s.userrepo.FindByID(ctx, id)
+	user, err := s.userrepo.FindByID(ctx, id)
+	if err != nil {
+		return nil, postgres_error.MapError(err, "get user by id", "user")
+	}
+	return user, nil
 }
 
 func (s *userService) List(ctx context.Context) ([]model.User, error) {
-	return s.userrepo.List(ctx)
+	users, err := s.userrepo.List(ctx)
+	if err != nil {
+		return nil, postgres_error.MapError(err, "list users", "user")
+	}
+	return users, nil
 }
 
 func (s *userService) UpdateDetails(ctx context.Context, user *model.User) error {
 	if err := services_validators.ValidateUpdateDetails(user); err != nil {
 		return err
 	}
-	return s.userrepo.UpdateDetails(ctx, user)
+	err := s.userrepo.UpdateDetails(ctx, user)
+	return postgres_error.MapError(err, "update user details", "user")
+
 }
 
 func (s *userService) Delete(ctx context.Context, id uuid.UUID) error {
 	if err := services_validators.ValidateDeleteID(id); err != nil {
 		return err
 	}
-	return s.userrepo.Delete(ctx, id)
+	return postgres_error.MapError(s.userrepo.Delete(ctx, id), "delete user", "user")
 }
