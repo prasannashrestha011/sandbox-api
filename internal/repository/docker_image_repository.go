d48c1f43708e0056dd1007309647d612a0d5a775
prasannashrestha011 <shresthaprashanna27@gmail.com>
2026-05-31 15:35:27 +0545

    feat: added list method to list all the docker images

--- internal/repository/docker_image_repository.go
+++ internal/repository/docker_image_repository.go
@@ -11,6 +11,7 @@ type DockerImageRepository interface {
 	// Create inserts a new Docker image record into the database.
 	Create(ctx context.Context, image *model.DockerImage) error
 	FindByID(ctx context.Context, id string) (*model.DockerImage, error)
+	List(ctx context.Context) ([]*model.DockerImage, error)
 }
 
 type dockerImageRepository struct {
@@ -33,4 +34,13 @@ func (r *dockerImageRepository) FindByID(ctx context.Context, id string) (*model
 		return nil, err
 	}
 	return &image, nil
+}
+
+func (r *dockerImageRepository) List(ctx context.Context) ([]*model.DockerImage, error) {
+	var images []*model.DockerImage
+	err := r.db.WithContext(ctx).Model(&model.DockerImage{}).Find(&images).Error
+	if err != nil {
+		return nil, err
+	}
+	return images, nil
 }
