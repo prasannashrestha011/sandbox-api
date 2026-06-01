c89a785d028dc365878bcc9044a5382fee2ec21c
prasannashrestha011 <shresthaprashanna27@gmail.com>
2026-06-01 17:09:46 +0545

    feat: added command builder method to build user command for execution

--- internal/sandbox/lang/commands.go
+++ internal/sandbox/lang/commands.go
@@ -1,6 +1,16 @@
 package lang
 
+import "fmt"
+
 var LangCommands = map[string][]string{
 	"python":     {"python", "-c"},
 	"javascript": {"node", "-e"},
+}
+
+func BuildCommand(lang string, code string) ([]string, error) {
+	cmdTemplate, ok := LangCommands[lang]
+	if !ok {
+		return nil, fmt.Errorf("unsupported language: %s", lang)
+	}
+	return append(cmdTemplate, code), nil
 }
