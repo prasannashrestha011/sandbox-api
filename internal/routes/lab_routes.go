7eef2fb0d64406cbe5606f9ff7ebc597d18b1184
prasannashrestha011 <shresthaprashanna27@gmail.com>
2026-06-04 18:22:08 +0545

    feat: added the  exercise routes

--- internal/routes/lab_routes.go
+++ internal/routes/lab_routes.go
@@ -9,7 +9,7 @@ import (
 )
 
 // RegisterLabRoutes wires lab endpoints into the router.
-func RegisterLabRoutes(r chi.Router, labController *controllers.LabController, chapterController *controllers.ChapterController) {
+func RegisterLabRoutes(r chi.Router, labController *controllers.LabController, chapterController *controllers.ChapterController, exerciseController *controllers.ExerciseHandler) {
 	r.Route("/labs", func(sr chi.Router) {
 		sr.Use(proxy.AuthMiddleware)
 		sr.Use(proxy.UserTypeMiddlware)
@@ -17,10 +17,18 @@ func RegisterLabRoutes(r chi.Router, labController *controllers.LabController, c
 		response.WrapGet(sr, "/{labId}", labController.GetLabByID)
 		response.WrapDelete(sr, "/{labId}", labController.DeleteLab)
 
-		sr.Route("/{id}/chapters", func(cr chi.Router) {
+		sr.Route("/{labId}/chapters", func(cr chi.Router) {
 			response.WrapPost(cr, "/", chapterController.CreateChapter)
 			response.WrapGet(cr, "/", chapterController.GetChaptersByLabID)
 			response.WrapPut(cr, "/{chapterId}", chapterController.UpdateChapter)
+
+			cr.Route("/{chapterId}/exercises", func(er chi.Router) {
+				response.WrapPost(er, "/", exerciseController.CreateExercise)
+				response.WrapGet(er, "/", exerciseController.ListExercisesByChapterID)
+				response.WrapGet(er, "/{exerciseId}", exerciseController.GetExerciseByID)
+				response.WrapPut(er, "/{exerciseId}", exerciseController.UpdateExercise)
+				response.WrapDelete(er, "/{exerciseId}", exerciseController.DeleteExercise)
+			})
 		})
 	})
 }
