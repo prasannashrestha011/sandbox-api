72030c30cd80a1117fd9d877ae43858012e06e1c
prasannashrestha011 <shresthaprashanna27@gmail.com>
2026-06-04 18:22:54 +0545

    feat: added exercise model for migration

--- internal/database/migration.go
+++ internal/database/migration.go
@@ -10,5 +10,5 @@ import (
 // AutoMigrate runs the schema migrations for persistence models.
 func AutoMigrate(db *gorm.DB) error {
 	return db.AutoMigrate(&model.Sandbox{}, &model.DockerImage{}, &model.User{}, &model.RefreshToken{},
-		&lab_model.Lab{}, &lab_model.Exercise{}, &lab_model.Chapter{}, &lab_model.Tag{})
+		&lab_model.Lab{}, &lab_model.Exercise{}, &lab_model.Chapter{}, &lab_model.Exercise{}, &lab_model.Tag{})
 }
