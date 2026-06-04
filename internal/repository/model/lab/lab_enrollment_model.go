290352e18730f750b46586257c3f2d13bf39279e
prasannashrestha011 <shresthaprashanna27@gmail.com>
2026-06-04 19:45:07 +0545

    feat: designed lab enrollment model to track enrolled users in a lab

---
+++ internal/repository/model/lab/lab_enrollment_model.go
@@ -0,0 +1,21 @@
+package lab_model
+
+import (
+	"main/internal/repository/model"
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type LabEnrollment struct {
+	ID          string     `gorm:"primaryKey;type:uuid"`
+	UserID      string     `gorm:"not null;index;type:uuid"`
+	LabID       string     `gorm:"not null;index;type:uuid"`
+	User        model.User `gorm:"foreignKey:UserID;references:UserID;constraint:OnDelete:CASCADE;"`
+	Lab         Lab        `gorm:"foreignKey:LabID;constraint:OnDelete:CASCADE;"`
+	Status      string     `gorm:"not null;default:'enrolled'"` // enrolled | in_progress | completed
+	ProgressPct int        `gorm:"not null;default:0"`
+	EnrolledAt  time.Time
+	CompletedAt *time.Time
+	DeletedAt   gorm.DeletedAt `gorm:"index"`
+}
