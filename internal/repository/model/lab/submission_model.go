ef570b32669b9a4371bd3352c4b5792fc6334e8a
prasannashrestha011 <shresthaprashanna27@gmail.com>
2026-06-05 21:27:13 +0545

    feat: gorm model for submission

--- internal/repository/model/lab/submission_model.go
+++ internal/repository/model/lab/submission_model.go
@@ -11,9 +11,12 @@ type Submission struct {
 	ID          string `gorm:"primaryKey;type:uuid"`
 	ExerciseID  string `gorm:"not null;index;type:uuid"`
 	UserID      string `gorm:"not null;index;type:uuid"`
+	Language    string `gorm:"not null"`
 	Code        string `gorm:"not null;type:text"`
 	Output      string `gorm:"type:text"`
 	Status      string `gorm:"not null"` // pending / passed / failed / error
+	Score       int    `gorm:"not null"`
+	AttemptNo   int    `gorm:"not null"`
 	SubmittedAt time.Time
 }
 
@@ -21,5 +24,6 @@ func (s *Submission) BeforeCreate(tx *gorm.DB) (err error) {
 	if s.ID == "" {
 		s.ID = uuid.New().String()
 	}
+	s.SubmittedAt = time.Now()
 	return nil
 }
