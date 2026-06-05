4d2eeab918836f88e34b2e2d3e44b10ad37faf7a
prasannashrestha011 <shresthaprashanna27@gmail.com>
2026-06-05 20:40:59 +0545

    feat: added enrollment routes, and refactored lab crud method to be accessible within instructor type only

--- internal/routes/lab_routes.go
+++ internal/routes/lab_routes.go
@@ -9,26 +9,38 @@ import (
 )
 
 // RegisterLabRoutes wires lab endpoints into the router.
-func RegisterLabRoutes(r chi.Router, labController *controllers.LabController, chapterController *controllers.ChapterController, exerciseController *controllers.ExerciseHandler) {
+func RegisterLabRoutes(r chi.Router, labController *controllers.LabController, chapterController *controllers.ChapterController,
+	exerciseController *controllers.ExerciseHandler, enrollmentController *controllers.EnrollmentController) {
 	r.Route("/labs", func(sr chi.Router) {
 		sr.Use(proxy.AuthMiddleware)
-		sr.Use(proxy.UserTypeMiddlware)
-		response.WrapPost(sr, "/", labController.CreateLab)
-		response.WrapGet(sr, "/{labId}", labController.GetLabByID)
-		response.WrapDelete(sr, "/{labId}", labController.DeleteLab)
+		//instructors routes
+		sr.Route("/", func(lr chi.Router) {
+			lr.Use(proxy.UserTypeMiddlware)
+			response.WrapPost(lr, "/", labController.CreateLab)
+			response.WrapGet(lr, "/{labId}", labController.GetLabByID)
+			response.WrapDelete(lr, "/{labId}", labController.DeleteLab)
 
-		sr.Route("/{labId}/chapters", func(cr chi.Router) {
-			response.WrapPost(cr, "/", chapterController.CreateChapter)
-			response.WrapGet(cr, "/", chapterController.GetChaptersByLabID)
-			response.WrapPut(cr, "/{chapterId}", chapterController.UpdateChapter)
+			lr.Route("/{labId}/chapters", func(cr chi.Router) {
+				response.WrapPost(cr, "/", chapterController.CreateChapter)
+				response.WrapGet(cr, "/", chapterController.GetChaptersByLabID)
+				response.WrapPut(cr, "/{chapterId}", chapterController.UpdateChapter)
 
-			cr.Route("/{chapterId}/exercises", func(er chi.Router) {
-				response.WrapPost(er, "/", exerciseController.CreateExercise)
-				response.WrapGet(er, "/", exerciseController.ListExercisesByChapterID)
-				response.WrapGet(er, "/{exerciseId}", exerciseController.GetExerciseByID)
-				response.WrapPut(er, "/{exerciseId}", exerciseController.UpdateExercise)
-				response.WrapDelete(er, "/{exerciseId}", exerciseController.DeleteExercise)
+				cr.Route("/{chapterId}/exercises", func(er chi.Router) {
+					response.WrapPost(er, "/", exerciseController.CreateExercise)
+					response.WrapGet(er, "/", exerciseController.ListExercisesByChapterID)
+					response.WrapGet(er, "/{exerciseId}", exerciseController.GetExerciseByID)
+					response.WrapPut(er, "/{exerciseId}", exerciseController.UpdateExercise)
+					response.WrapDelete(er, "/{exerciseId}", exerciseController.DeleteExercise)
+				})
 			})
 		})
+		//students routes
+		sr.Route("/enrollments", func(er chi.Router) {
+			response.WrapPost(er, "/", enrollmentController.EnrollUserToLab)
+			response.WrapGet(er, "/{labId}", enrollmentController.GetEnrollment)
+			response.WrapGet(er, "/user/{userId}", enrollmentController.GetUserEnrollments)
+			response.WrapDelete(er, "/{labId}", enrollmentController.DeleteEnrollment)
+		})
+
 	})
 }
