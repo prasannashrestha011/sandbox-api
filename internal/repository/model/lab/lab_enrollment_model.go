5857ba302d6a03a130a2e630fb2b8f07b0ffd84a
prasannashrestha011 <shresthaprashanna27@gmail.com>
2026-06-05 20:41:12 +0545

    fix: updated LabEnrollment model to use EnrolledUserID for foreign key reference and ensure unique enrollment per user and lab

--- internal/repository/model/lab/lab_enrollment_model.go
+++ internal/repository/model/lab/lab_enrollment_model.go
@@ -9,16 +9,16 @@ import (
 )
 
 type LabEnrollment struct {
-	ID          string     `gorm:"primaryKey;type:uuid"`
-	UserID      string     `gorm:"not null;index;type:uuid"`
-	LabID       string     `gorm:"not null;index;type:uuid"`
-	User        model.User `gorm:"foreignKey:UserID;references:UserID;constraint:OnDelete:CASCADE;"`
-	Lab         Lab        `gorm:"foreignKey:LabID;constraint:OnDelete:CASCADE;"`
-	Status      string     `gorm:"not null;default:'enrolled'"` // enrolled | in_progress | completed
-	ProgressPct int        `gorm:"not null;default:0"`
-	EnrolledAt  time.Time
-	CompletedAt *time.Time
-	DeletedAt   gorm.DeletedAt `gorm:"index"`
+	ID             string     `gorm:"primaryKey;type:uuid"`
+	EnrolledUserID string     `gorm:"not null;type:uuid;uniqueIndex:uniq_user_lab"`
+	User           model.User `gorm:"foreignKey:EnrolledUserID;references:UserID;constraint:OnDelete:CASCADE;"`
+	LabID          string     `gorm:"not null;type:uuid;uniqueIndex:uniq_user_lab"`
+	Lab            Lab        `gorm:"foreignKey:LabID;constraint:OnDelete:CASCADE;"`
+	Status         string     `gorm:"not null;default:'enrolled'"` // enrolled | in_progress | completed
+	ProgressPct    int        `gorm:"not null;default:0"`
+	EnrolledAt     time.Time
+	CompletedAt    *time.Time
+	DeletedAt      gorm.DeletedAt `gorm:"index"`
 }
 
 func (l *LabEnrollment) BeforeCreate(tx *gorm.DB) (err error) {
