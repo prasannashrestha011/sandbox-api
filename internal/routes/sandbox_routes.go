63163c9ef2e34d00c41f33babbc4f0ec50ac1b69
prasannashrestha011 <shresthaprashanna27@gmail.com>
2026-06-11 19:12:15 +0545

    feat(sandbox_template): define sandbox template routes

--- internal/routes/sandbox_routes.go
+++ internal/routes/sandbox_routes.go
@@ -10,13 +10,11 @@ import (
 
 // RegisterSandboxRoutes wires sandbox endpoints into the router.
 func RegisterSandboxRoutes(r chi.Router, controller *controllers.SandboxController) {
-	r.Route("/sandboxes", func(sr chi.Router) {
+	r.Route("/sandbox-template", func(sr chi.Router) {
 		sr.Use(proxy.AuthMiddleware)
 		response.WrapPost(sr, "/", controller.CreateSandbox)
 		response.WrapGet(sr, "/{id}", controller.GetSandboxByID)
-		response.WrapGet(sr, "/", controller.ListSandboxesByUser)
-		response.WrapGet(sr, "/session/{sessionId}", controller.GetSandboxBySessionID)
-		response.WrapPost(sr, "/{id}/execute", controller.ExecuteCode)
-		response.WrapDelete(sr, "/{id}", controller.DeleteSandbox)
+		response.WrapGet(sr, "/", controller.ListTemplatesByUser)
+		response.WrapPut(sr, "/{id}", controller.UpdateSandbox)
 	})
 }
