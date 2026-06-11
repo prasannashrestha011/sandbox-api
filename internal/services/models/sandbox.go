da791c6c090f1cbec7c9181c5ae4cd46e5dc9632
prasannashrestha011 <shresthaprashanna27@gmail.com>
2026-06-11 19:14:25 +0545

    feat: defined sandbox model for template, session & execution

--- internal/services/models/sandbox.go
+++ internal/services/models/sandbox.go
@@ -3,14 +3,13 @@ package models
 import (
 	"main/internal/enums"
 	"time"
-
-	"github.com/google/uuid"
 )
 
-type Sandbox struct {
-	ID             uuid.UUID
+// sandbox blueprint
+type SandboxTemplate struct {
+	ID             string
 	UserID         string
-	Environment    string
+	Lang           string
 	Image          DockerImage
 	MemoryLimit    int64
 	CPULimit       int64
@@ -18,13 +17,34 @@ type Sandbox struct {
 	SessionTimeout time.Duration
 	ExecTimeout    time.Duration
 	NetworkMode    string
+}
+type SandboxSession struct {
+	ID         string
+	TemplateID string
+	UserID     string
 
-	ContainerName string
 	ContainerID   string
-	SessionID     uuid.UUID
-	Status        enums.SandboxState
-	ExpiresAt     time.Time
+	ContainerName string
 
+	Lang   string
+	Status enums.SandboxState
+
+	SessionTimeout time.Duration
+	ExecTimeout    time.Duration
+	ExpiresAt      time.Time
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
+}
+
+type SandboxExecution struct {
+	ID        string
+	UserID    string
+	SessionID string
+
+	Command   string
+	Stdout    string
+	Stderr    string
+	ExitCode  int
+	ExecTime  time.Duration
 	CreatedAt time.Time
-	UpdatedAt time.Time
 }
