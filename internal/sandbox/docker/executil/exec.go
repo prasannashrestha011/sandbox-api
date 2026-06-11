4df020d91862a1957d6bd167acb13584bb24fbd2
prasannashrestha011 <shresthaprashanna27@gmail.com>
2026-06-11 19:23:39 +0545

    feat(docker-client): returned structured response from docker exec result

--- internal/sandbox/docker/executil/exec.go
+++ internal/sandbox/docker/executil/exec.go
@@ -3,30 +3,42 @@ package sb_executil
 import (
 	"bytes"
 	"context"
+	"main/internal/dto"
 
 	"github.com/moby/moby/api/pkg/stdcopy"
 	"github.com/moby/moby/client"
 )
 
-func ExecCreate(ctx context.Context, apiClient *client.Client, containerID string, cmd []string) (string, error) {
+func ExecCreate(ctx context.Context, apiClient *client.Client, containerID string, cmd []string) (*dto.SandboxExecResponse, error) {
 	execID, err := apiClient.ExecCreate(ctx, containerID, client.ExecCreateOptions{
 		AttachStdout: true,
 		AttachStderr: true,
 		Cmd:          cmd,
 	})
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
 	resp, err := apiClient.ExecAttach(ctx, execID.ID, client.ExecAttachOptions{})
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 	defer resp.Close()
 
-	var buf bytes.Buffer
+	var stdoutBuf bytes.Buffer
+	var stderrBuf bytes.Buffer
+	_, err = stdcopy.StdCopy(&stdoutBuf, &stderrBuf, resp.Reader)
+	if err != nil {
+		return nil, err
+	}
+	inspect, err := apiClient.ExecInspect(ctx, execID.ID, client.ExecInspectOptions{})
+	if err != nil {
+		return nil, err
+	}
 
-	stdcopy.StdCopy(&buf, &buf, resp.Reader)
-	println(buf.String())
-	return buf.String(), nil
+	return &dto.SandboxExecResponse{
+		Stdout:   stdoutBuf.String(),
+		Stderr:   stderrBuf.String(),
+		ExitCode: inspect.ExitCode,
+	}, nil
 }
