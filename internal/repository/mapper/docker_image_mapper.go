58f0eea54e5088db61edeb2594b760f2956d3fb3
prasannashrestha011 <shresthaprashanna27@gmail.com>
2026-06-18 20:24:03 +0545

    feat(docker_image): defined service & gorm layer mapper

---
+++ internal/repository/mapper/docker_image_mapper.go
@@ -0,0 +1,48 @@
+package mapper
+
+import (
+	"main/internal/repository/model"
+	"main/internal/services/models"
+)
+
+func DockerImageToGom(d *models.DockerImage) *model.DockerImage {
+	if d == nil {
+		return nil
+	}
+
+	return &model.DockerImage{
+		ID:          d.ID,
+		ImageTag:    d.ImageTag,
+		Runtime:     d.Runtime,
+		CreatedByID: d.CreatedByID,
+		CreatedAt:   d.CreatedAt,
+		UpdatedAt:   d.UpdatedAt,
+	}
+}
+
+func DockerImageFromGom(d *model.DockerImage) *models.DockerImage {
+	if d == nil {
+		return nil
+	}
+
+	return &models.DockerImage{
+		ID:          d.ID,
+		ImageTag:    d.ImageTag,
+		Runtime:     d.Runtime,
+		CreatedByID: d.CreatedByID,
+		CreatedAt:   d.CreatedAt,
+		UpdatedAt:   d.UpdatedAt,
+	}
+}
+
+func DockerImagesFromGom(d []*model.DockerImage) []*models.DockerImage {
+	if d == nil {
+		return nil
+	}
+
+	images := make([]*models.DockerImage, len(d))
+	for i, v := range d {
+		images[i] = DockerImageFromGom(v)
+	}
+	return images
+}
