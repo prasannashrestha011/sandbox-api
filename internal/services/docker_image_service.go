04404d0ed281190a72daeda49423840a74c5c459
prasannashrestha011 <shresthaprashanna27@gmail.com>
2026-06-18 20:24:17 +0545

    refactor(docker_image_service): update CreateImage and ListImages methods to use DTOs and context

--- internal/services/docker_image_service.go
+++ internal/services/docker_image_service.go
@@ -3,18 +3,17 @@ package services
 import (
 	"context"
 	"fmt"
+	"main/internal/dto"
 	postgres_error "main/internal/infra/postgres"
 	"main/internal/repository"
-	"main/internal/repository/model"
 	"main/internal/sandbox/core"
-	"path"
-	"strings"
+	"main/internal/services/mapper"
 )
 
 type DockerImageService interface {
 	// CreateImage creates a new Docker image record in the database.
-	CreateImage(imageTag string, userID string) error
-	ListImages() ([]*model.DockerImage, error)
+	CreateImage(ctx context.Context, req *dto.CreateImageRequest) error
+	ListImages(ctx context.Context) ([]*dto.DockerImageResponse, error)
 }
 
 type dockerImageService struct {
@@ -26,28 +25,11 @@ func NewDockerImageService(repo repository.DockerImageRepository, sandboxClient 
 	return &dockerImageService{repo: repo, sandboxClient: sandboxClient}
 }
 
-func langFromImageTag(imageTag string) (string, error) {
-	// imageTag format: "language:version" or "registry/language:version"
-	base := path.Base(imageTag) // strips registry prefix if any
-	lang, _, found := strings.Cut(base, ":")
-	if !found {
-		return "", fmt.Errorf("invalid image tag format: %s", imageTag)
-	}
-	return lang, nil
-}
-func (s *dockerImageService) CreateImage(imageTag string, userID string) error {
-	err := s.sandboxClient.PullImage(context.Background(), imageTag)
+func (s *dockerImageService) CreateImage(ctx context.Context, req *dto.CreateImageRequest) error {
+	dockerImage := mapper.ToDockerImageModel(ctx, req)
+	err := s.sandboxClient.PullImage(context.Background(), dockerImage.ImageTag)
 	if err != nil {
-		return err
-	}
-	lang, err := langFromImageTag(imageTag)
-	if err != nil {
-		return fmt.Errorf("failed to extract language from image tag %s: %w", imageTag, err)
-	}
-	dockerImage := &model.DockerImage{
-		ImageTag:    imageTag,
-		CreatedByID: userID,
-		Lang:        lang,
+		return fmt.Errorf("failed to extract language from image tag %s: %w", req.ImageTag, err)
 	}
 	err = s.repo.Create(context.Background(), dockerImage)
 	if err != nil {
@@ -56,10 +38,10 @@ func (s *dockerImageService) CreateImage(imageTag string, userID string) error {
 	return nil
 }
 
-func (s *dockerImageService) ListImages() ([]*model.DockerImage, error) {
-	list, err := s.repo.List(context.Background())
+func (s *dockerImageService) ListImages(ctx context.Context) ([]*dto.DockerImageResponse, error) {
+	list, err := s.repo.List(ctx)
 	if err != nil {
 		return nil, postgres_error.MapError(err, "listing Docker images", "docker_image")
 	}
-	return list, nil
+	return mapper.ToDockerImageResponses(list), nil
 }
