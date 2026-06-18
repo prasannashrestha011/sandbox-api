afa54b4be0d7cfac03d2904ea9afd65d011d193f
prasannashrestha011 <shresthaprashanna27@gmail.com>
2026-06-18 19:49:04 +0545

    feat: implemented execution timeout. The time limit is set to 30 second. If time exceeded, the container process will be killed

--- internal/sandbox/docker/executil/exec.go
+++ internal/sandbox/docker/executil/exec.go
@@ -3,14 +3,19 @@ package sb_executil
 import (
 	"bytes"
 	"context"
+	"fmt"
+	"main/internal/domain"
 	"main/internal/dto"
+	"time"
 
 	"github.com/moby/moby/api/pkg/stdcopy"
 	"github.com/moby/moby/client"
 )
 
 func ExecCreate(ctx context.Context, apiClient *client.Client, containerID string, cmd []string) (*dto.SandboxExecResponse, error) {
-	execID, err := apiClient.ExecCreate(ctx, containerID, client.ExecCreateOptions{
+	exeCtx, cancel := context.WithTimeout(ctx, 30*time.Second)
+	defer cancel()
+	execID, err := apiClient.ExecCreate(exeCtx, containerID, client.ExecCreateOptions{
 		AttachStdout: true,
 		AttachStderr: true,
 		Cmd:          cmd,
@@ -19,19 +24,36 @@ func ExecCreate(ctx context.Context, apiClient *client.Client, containerID strin
 		return nil, err
 	}
 
-	resp, err := apiClient.ExecAttach(ctx, execID.ID, client.ExecAttachOptions{})
+	resp, err := apiClient.ExecAttach(exeCtx, execID.ID, client.ExecAttachOptions{})
 	if err != nil {
 		return nil, err
 	}
 	defer resp.Close()
 
+	copyDone := make(chan error, 1)
+
 	var stdoutBuf bytes.Buffer
 	var stderrBuf bytes.Buffer
-	_, err = stdcopy.StdCopy(&stdoutBuf, &stderrBuf, resp.Reader)
-	if err != nil {
-		return nil, err
+	go func() {
+		_, copyErr := stdcopy.StdCopy(&stdoutBuf, &stderrBuf, resp.Reader)
+		copyDone <- copyErr
+	}()
+
+	select {
+	case err := <-copyDone:
+		if err != nil {
+			return nil, err
+		}
+	case <-exeCtx.Done():
+		inspect, err := apiClient.ExecInspect(context.Background(), execID.ID, client.ExecInspectOptions{})
+		if err == nil && inspect.PID != 0 {
+			killCmd := []string{"kill", "-9", fmt.Sprintf("%d", inspect.PID)}
+			killExec, _ := apiClient.ExecCreate(context.Background(), containerID, client.ExecCreateOptions{Cmd: killCmd})
+			apiClient.ExecStart(ctx, killExec.ID, client.ExecStartOptions{})
+		}
+		return nil, domain.InvalidRequestError("execution time out", nil)
 	}
-	inspect, err := apiClient.ExecInspect(ctx, execID.ID, client.ExecInspectOptions{})
+	inspect, err := apiClient.ExecInspect(context.Background(), execID.ID, client.ExecInspectOptions{})
 	if err != nil {
 		return nil, err
 	}
