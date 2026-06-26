5079f09925234542fa2cd909521b432980b9e3e7
prasannashrestha011 <shresthaprashanna27@gmail.com>
2026-06-26 13:18:57 +0545

    added comment description

--- internal/jobs/client.go
+++ internal/jobs/client.go
@@ -2,6 +2,7 @@ package jobs
 
 import "github.com/hibiken/asynq"
 
+// called in app.go to initialize the asynq client
 func InitAsynq() *asynq.Client {
 	client := asynq.NewClient(asynq.RedisClientOpt{
 		Addr:     "127.0.0.1:6379",
