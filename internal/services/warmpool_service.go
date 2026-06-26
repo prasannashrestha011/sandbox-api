e0740633036b5455732a60d7cf2073f238e30649
prasannashrestha011 <shresthaprashanna27@gmail.com>
2026-06-26 11:10:51 +0545

    feat(warmpool): enhance CreateWarmpool method to enqueue sandbox provisioning tasks

--- internal/services/warmpool_service.go
+++ internal/services/warmpool_service.go
@@ -1,10 +1,14 @@
 package services
 
 import (
+	"log"
 	"main/internal/dto"
 	postgres_error "main/internal/infra/postgres"
+	"main/internal/jobs/warmpool"
 	repository "main/internal/repository"
 	"main/internal/services/mapper"
+
+	"github.com/hibiken/asynq"
 )
 
 type WarmpoolService interface {
@@ -12,11 +16,13 @@ type WarmpoolService interface {
 }
 type warmpoolService struct {
 	warmpoolRepo repository.WarmPoolRepository
+	asyncClient  *asynq.Client
 }
 
-func NewWarmpoolService(warmpoolRepo repository.WarmPoolRepository) WarmpoolService {
+func NewWarmpoolService(warmpoolRepo repository.WarmPoolRepository, asyncClient *asynq.Client) WarmpoolService {
 	return &warmpoolService{
 		warmpoolRepo: warmpoolRepo,
+		asyncClient:  asyncClient,
 	}
 }
 func (w *warmpoolService) CreateWarmpool(req *dto.CreateWarmPoolRequest) (*dto.WarmPoolResponse, error) {
@@ -25,5 +31,20 @@ func (w *warmpoolService) CreateWarmpool(req *dto.CreateWarmPoolRequest) (*dto.W
 	if err != nil {
 		return nil, postgres_error.MapError(err, "create warmpool", "warmpool")
 	}
+
+	scalingPolicy := mapper.ToScalingPolicyModel(req, warmPool.ID)
+	payload := &dto.SandboxProvisionPayload{
+		TemplateID: createdWarmPool.TemplateID,
+		PoolID:     createdWarmPool.ID,
+	}
+	task, _ := warmpool.NewSandboxProvisionTask(payload)
+	for i := 0; i < scalingPolicy.MinIdleThreshold; i++ {
+		log.Println("Enqueuing task to provision sandbox for warm pool:", createdWarmPool.ID, "Template:", createdWarmPool.TemplateID)
+		_, err = w.asyncClient.Enqueue(task)
+		if err != nil {
+			return nil, err
+		}
+	}
+
 	return mapper.ToWarmPoolResponse(createdWarmPool), nil
 }
